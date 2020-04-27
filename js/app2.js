5793ae1c764609059a8e4c508def2835138e6947
habtomab <47911370+habtomab@users.noreply.github.com>
2020-04-27 03:08:28 -0500

    su

--- js/app2.js
+++ js/app2.js
@@ -4,6 +4,6 @@ document.getElementById("button").onclick = function(){
         let date = new Date();
         let dateString = date.toString();//date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + ":" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
         document.getElementById("clock").innerHTML = dateString;
-        document.getElementById("image").src ="image/giphy3.gif ";
+        document.getElementById("image").src ="image/giphy3.gif";
     }, 1000);    
 }
